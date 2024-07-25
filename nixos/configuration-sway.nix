4c7228fa6e5f084c70226a3a8a0957e28a008455
Levi Doucette <vuul_ax@yahoo.com>
2024-07-25 23:03:27 +0000

    Update configuration-sway.nix

--- nixos/configuration-sway.nix
+++ nixos/configuration-sway.nix
@@ -84,22 +84,18 @@
     librewolf
     greetd.greetd
     greetd.tuigreet
+    swaylock
+    swayidle
+    wl-clipboard
+    mako
+    grim
+    slurp
+    dmenu
   ];
 
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true; # so that gtk works properly
-    extraPackages = with pkgs; [
-      swaylock
-      swayidle
-      wl-clipboard
-      wf-recorder
-      mako # notification daemon
-      grim
-      slurp
-      alacritty # Alacritty is the default terminal in the config
-      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
-    ];
     extraSessionCommands = ''
       export SDL_VIDEODRIVER=wayland
       export _JAVA_AWT_WM_NONREPARENTING=1
